a6fb92ed4b41db8518b225b1cec4a79506affb45
Marco Arriaga <marriaga158@gmail.com>
2022-11-12 18:43:43 -0500

    feat: finalize address bar

--- src/components/ReportDialog.js
+++ src/components/ReportDialog.js
@@ -4,33 +4,35 @@ import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
-import { Input, TextField } from '@mui/material';
+// TODO: put all the imports inside this
+import { TextField } from '@mui/material';
+// import { ThemeProvider, createTheme } from '@mui/system';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import Close from '@mui/icons-material/Close';
 // import { usePlacesWidget } from 'react-google-autocomplete';
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+// import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 import { DateTimePicker } from '@mui/x-date-pickers';
-import Autocomplete from '@mui/material/Autocomplete';
+// import Autocomplete from '@mui/material/Autocomplete';
+import { Autocomplete } from '@react-google-maps/api';
 
-var currentDate = new Date();
+function textHandler(){
+
+}
 
 
 const ReportDialog = ({ open, onClose }) => {
-//   const { ref } = usePlacesWidget({
-// 	apiKey:'AIzaSyA2EahkvqFTyxK4Taak5jkgQmgLbsdPzq0',
-// 	onPlaceSelected: (place) => {
-// 		console.log(place);
-// 	},
-// 	options: {
-// 		types: ["(regions)"],
-// 	}
-//   });
 
   return (
 	<Dialog 
 		open={open}
 		onClose={onClose}
+		style={{
+			// the dropdown is next to the dialog root, not inside
+			"& + .pac-container": {
+				zIndex: 9999
+			}
+		}}
 	>
 		<DialogTitle
 			sx={{
@@ -53,12 +55,36 @@ const ReportDialog = ({ open, onClose }) => {
 					marginBottom: '5px'
 				}}
 			>
-				Enter a title of the incident that will appear on the map.
+				Enter the date and time the incident took place.
 			</Typography>
 			<DateTimePicker
 				onChange={() => {}}
 				renderInput={(params) => <TextField fullWidth {...params} />}
 			/>
+			<Typography
+				sx={{
+					marginTop: '20px',
+					marginBottom: '5px'
+				}}
+			>
+				Enter the address of the incident.
+			</Typography>
+			{/* TODO: replace this with something better, the react-google-autocomplete
+			is more robust but I can't understand how the MaterialUI example works */}
+			{/* 
+				apiKey={'AIzaSyA2EahkvqFTyxK4Taak5jkgQmgLbsdPzq0'}
+				for some reason this doesn't require the api key?
+			*/}
+			<Autocomplete
+				// onLoad={this.onLoad}
+				onPlacechanged={() => {console.log('place changed')}}
+			>
+				<TextField
+					fullWidth
+					// variant="outline"
+					// label="Address"
+				/>
+			</Autocomplete>
 			<Typography
 				sx={{
 					marginTop: '20px',
@@ -85,19 +111,6 @@ const ReportDialog = ({ open, onClose }) => {
 				fullWidth
 				multiline
 				rows={4}
-			/>
-			<Typography
-				sx={{
-					marginTop: '20px',
-					marginBottom: '5px'
-				}}
-			>
-				Enter the address of the incident.
-			</Typography>
-			{/* TODO: replace this with something better, the react-google-autocomplete
-			is more robust but I can't understand how the MaterialUI example works */}
-			<GooglePlacesAutocomplete
-				apiKey={'AIzaSyA2EahkvqFTyxK4Taak5jkgQmgLbsdPzq0'}
 			/>
 		</DialogContent>
 	</Dialog>
