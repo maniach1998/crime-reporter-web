30fa61b05d6dde90388432a5df23c2eaed388f89
Manas Acharekar <manas.acharekar98@gmail.com>
2022-12-03 20:27:15 -0500

    backend queries from CrimeOMeter API instead of using mock data

--- backend/routes/report.js
+++ backend/routes/report.js
@@ -1,28 +1,13 @@
 const router = require('express').Router();
 const Report = require('../models/CrimeReport');
+const fetch = require('node-fetch');
+const moment = require('moment');
 
 const mockData = require('../data/mockData');
 
 router.get('/', async (req, res) => {
 	const lng = Number(req.query.lng);
 	const lat = Number(req.query.lat);
-
-	const apiResponse = mockData.incidents.map(
-		(crime) =>
-			new Report({
-				address: crime.incident_address,
-				title: crime.incident_offense,
-				description: crime.incident_offense_detail_description,
-				reportedAt: crime.incident_date,
-				location: {
-					type: 'Point',
-					coordinates: [
-						crime.incident_longitude,
-						crime.incident_latitude,
-					],
-				},
-			})
-	);
 
 	const reports = await Report.aggregate([
 		{
@@ -38,16 +23,56 @@ router.get('/', async (req, res) => {
 		},
 	]);
 
-	// merge crime arrays and sort to show latest crimes
-	const data = apiResponse
-		.concat(reports)
-		.sort(
-			(a, b) =>
-				new Date(b.reportedAt).getTime() -
-				new Date(a.reportedAt).getTime()
+	const now = moment(moment.now()).format('YYYY-MM-DD HH:mm:ss').toString();
+	const aWeekAgo = moment(moment.now())
+		.day(-7)
+		.startOf('day')
+		.format('YYYY-MM-DD HH:mm:ss')
+		.toString();
+	const distance = '10mi';
+
+	const apiUrl = `https://api.crimeometer.com/v1/incidents/raw-data?lat=${lat}&lon=${lng}&distance=${distance}&datetime_ini=${now}&datetime_end=${aWeekAgo}&page=1`;
+
+	const apiRes = await fetch(apiUrl, {
+		headers: {
+			'Content-Type': 'application/json',
+			'x-api-key': process.env.API_KEY,
+		},
+	});
+	const response = await apiRes.json();
+
+	// if the API responds with data
+	if (response.total_incidents !== 0) {
+		const apiResponse = response.incidents.map(
+			(crime) =>
+				new Report({
+					address: crime.incident_address,
+					title: crime.incident_offense,
+					description: crime.incident_offense_detail_description,
+					reportedAt: crime.incident_date,
+					location: {
+						type: 'Point',
+						coordinates: [
+							crime.incident_longitude,
+							crime.incident_latitude,
+						],
+					},
+				})
 		);
 
-	res.send({ data });
+		// merge crime arrays and sort to show latest crimes
+		const data = apiResponse
+			.concat(reports)
+			.sort(
+				(a, b) =>
+					new Date(b.reportedAt).getTime() -
+					new Date(a.reportedAt).getTime()
+			);
+
+		res.send({ data });
+	} else {
+		res.send({ data: reports });
+	}
 });
 
 router.post('/new-report', async (req, res) => {
